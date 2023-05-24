2063534283f553298ea9f1b1e045bd33ff9a2240
framadhan99 <fajarramadhan99704@gmail.com>
2023-05-24 07:53:47 +0700

    fix login and sign up

--- lib/ui_features/pages/login_page.dart
+++ lib/ui_features/pages/login_page.dart
@@ -18,91 +18,97 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: screenWidth(context) * 0.1),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            verticalSpace(300),
-            Text(
-              'Log in',
-              style: AssetStyles.mediumTitle.copyWith(
-                fontWeight: FontWeight.w700,
-                fontSize: 28,
-                color: AssetColors.greenDark,
-              ),
-            ),
-            verticalSpace(30),
-            Text(
-              'Username',
-              style: AssetStyles.smallTitle.copyWith(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: AssetColors.greenDark,
-              ),
-            ),
-            verticalSpace(6),
-            const TextFieldGeneral(),
-            verticalSpace(9),
-            Text(
-              'Password',
-              style: AssetStyles.smallTitle.copyWith(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: AssetColors.greenDark,
-              ),
-            ),
-            verticalSpace(6),
-            const TextFieldGeneral(),
-            verticalSpace(5),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                'Forgot Password?',
-                style: AssetStyles.smallDescription
-                    .copyWith(color: AssetColors.orangePastel),
-              ),
-            ),
-            verticalSpace(50),
-            const Align(
-              alignment: Alignment.center,
-              child: ButtonPrimary(
-                label: 'Log in',
-                width: 114,
-              ),
-            ),
-            verticalSpace(63),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                logos.length,
-                (index) => Image.asset(logos[index]),
-              ),
-            ),
-            verticalSpace(125),
-            Row(
+        child: Center(
+          child: SizedBox(
+            height: screenHeight(context),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                verticalSpace(100),
                 Text(
-                  'Have an account ?',
+                  'Log in',
+                  style: AssetStyles.mediumTitle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 28,
+                    color: AssetColors.greenDark,
+                  ),
+                ),
+                verticalSpace(30),
+                Text(
+                  'Username',
                   style: AssetStyles.smallTitle.copyWith(
-                    color: AssetColors.grey,
-                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: AssetColors.greenDark,
                   ),
                 ),
-                horizontalSpace(10),
-                GestureDetector(
-                  onTap: () => nextScreen(const SignupPage()),
+                verticalSpace(6),
+                const TextFieldGeneral(),
+                verticalSpace(9),
+                Text(
+                  'Password',
+                  style: AssetStyles.smallTitle.copyWith(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: AssetColors.greenDark,
+                  ),
+                ),
+                verticalSpace(6),
+                const TextFieldGeneral(),
+                verticalSpace(5),
+                Align(
+                  alignment: Alignment.centerRight,
                   child: Text(
-                    'Sign up',
-                    style: AssetStyles.smallTitle.copyWith(
-                      color: AssetColors.orangePastel,
-                      fontWeight: FontWeight.w400,
-                    ),
+                    'Forgot Password?',
+                    style: AssetStyles.smallDescription
+                        .copyWith(color: AssetColors.orangePastel),
                   ),
                 ),
+                verticalSpace(50),
+                const Align(
+                  alignment: Alignment.center,
+                  child: ButtonPrimary(
+                    label: 'Log in',
+                    width: 114,
+                  ),
+                ),
+                verticalSpace(63),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(
+                    logos.length,
+                    (index) => Image.asset(logos[index]),
+                  ),
+                ),
+                verticalSpace(125),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Have an account ?',
+                      style: AssetStyles.smallTitle.copyWith(
+                        color: AssetColors.grey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    horizontalSpace(10),
+                    GestureDetector(
+                      onTap: () => nextScreen(const SignupPage()),
+                      child: Text(
+                        'Sign up',
+                        style: AssetStyles.smallTitle.copyWith(
+                          color: AssetColors.orangePastel,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // verticalSpace(50),
               ],
             ),
-            verticalSpace(50),
-          ],
+          ),
         ),
       ),
     );
--- lib/ui_features/pages/signup_page.dart
+++ lib/ui_features/pages/signup_page.dart
@@ -18,127 +18,133 @@ class SignupPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: screenWidth(context) * 0.1),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            verticalSpace(100),
-            Text(
-              'Sign up',
-              style: AssetStyles.mediumTitle.copyWith(
-                fontWeight: FontWeight.w700,
-                fontSize: 28,
-                color: AssetColors.greenDark,
-              ),
-            ),
-            verticalSpace(30),
-            Text(
-              'Username',
-              style: AssetStyles.smallTitle.copyWith(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: AssetColors.greenDark,
-              ),
-            ),
-            verticalSpace(6),
-            const TextFieldGeneral(),
-            verticalSpace(9),
-            Text(
-              'Password',
-              style: AssetStyles.smallTitle.copyWith(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: AssetColors.greenDark,
-              ),
-            ),
-            verticalSpace(6),
-            const TextFieldGeneral(),
-            verticalSpace(9),
-            Text(
-              'Confirm password',
-              style: AssetStyles.smallTitle.copyWith(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: AssetColors.greenDark,
-              ),
-            ),
-            verticalSpace(6),
-            const TextFieldGeneral(),
-            verticalSpace(50),
-            const Align(
-              alignment: Alignment.center,
-              child: ButtonPrimary(
-                label: 'Create account',
-                width: 162,
-              ),
-            ),
-            verticalSpace(36),
-            SizedBox(
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  const Divider(),
-                  Container(
-                    width: 17,
-                    color: Colors.white,
-                    alignment: Alignment.center,
-                    child: Text(
-                      'or',
-                      style: AssetStyles.smallDescription.copyWith(
-                        color: const Color(0xffE0E0E0),
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        height: 1,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            verticalSpace(12),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                'Continue with',
-                style: AssetStyles.mediumTitle.copyWith(
-                  fontSize: 14,
-                  color: AssetColors.orangePastel,
-                ),
-              ),
-            ),
-            verticalSpace(22),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                logos.length,
-                (index) => Image.asset(logos[index]),
-              ),
-            ),
-            verticalSpace(100),
-            Row(
+        child: Center(
+          child: SizedBox(
+            height: screenHeight(context),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                verticalSpace(100),
                 Text(
-                  'Have an account ?',
-                  style: AssetStyles.smallTitle.copyWith(
-                    color: AssetColors.grey,
-                    fontWeight: FontWeight.w400,
+                  'Sign up',
+                  style: AssetStyles.mediumTitle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 28,
+                    color: AssetColors.greenDark,
                   ),
                 ),
-                horizontalSpace(10),
-                GestureDetector(
-                  onTap: () => nextScreen(const LoginPage()),
+                verticalSpace(30),
+                Text(
+                  'Username',
+                  style: AssetStyles.smallTitle.copyWith(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: AssetColors.greenDark,
+                  ),
+                ),
+                verticalSpace(6),
+                const TextFieldGeneral(),
+                verticalSpace(9),
+                Text(
+                  'Password',
+                  style: AssetStyles.smallTitle.copyWith(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: AssetColors.greenDark,
+                  ),
+                ),
+                verticalSpace(6),
+                const TextFieldGeneral(),
+                verticalSpace(9),
+                Text(
+                  'Confirm password',
+                  style: AssetStyles.smallTitle.copyWith(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: AssetColors.greenDark,
+                  ),
+                ),
+                verticalSpace(6),
+                const TextFieldGeneral(),
+                verticalSpace(50),
+                const Align(
+                  alignment: Alignment.center,
+                  child: ButtonPrimary(
+                    label: 'Create account',
+                    width: 162,
+                  ),
+                ),
+                verticalSpace(36),
+                SizedBox(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      const Divider(),
+                      Container(
+                        width: 17,
+                        color: Colors.white,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'or',
+                          style: AssetStyles.smallDescription.copyWith(
+                            color: const Color(0xffE0E0E0),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            height: 1,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                verticalSpace(12),
+                Align(
+                  alignment: Alignment.center,
                   child: Text(
-                    'Log in',
-                    style: AssetStyles.smallTitle.copyWith(
+                    'Continue with',
+                    style: AssetStyles.mediumTitle.copyWith(
+                      fontSize: 14,
                       color: AssetColors.orangePastel,
-                      fontWeight: FontWeight.w400,
                     ),
                   ),
                 ),
+                verticalSpace(22),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(
+                    logos.length,
+                    (index) => Image.asset(logos[index]),
+                  ),
+                ),
+                verticalSpace(100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Have an account ?',
+                      style: AssetStyles.smallTitle.copyWith(
+                        color: AssetColors.grey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    horizontalSpace(10),
+                    GestureDetector(
+                      onTap: () => nextScreen(const LoginPage()),
+                      child: Text(
+                        'Log in',
+                        style: AssetStyles.smallTitle.copyWith(
+                          color: AssetColors.orangePastel,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // verticalSpace(50)
               ],
             ),
-            verticalSpace(50)
-          ],
+          ),
         ),
       ),
     );
